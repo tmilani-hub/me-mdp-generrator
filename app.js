145277c7e479ed9e35b88e054b9d5d55b2b48681
tmilani-hub <tmilani@edenschool.fr>
2026-03-27 04:30:04 +0100

    i want to sleep

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-window.scroll({
-	top: 100,
-	left: 100,
-	behavior: "smooth",
-});
+/*
+const $ = document.querySelector("")
+*/
+const $img = document.querySelector("img")
+let $btns = document.querySelectorAll("button")
+let $as = document.querySelectorAll("a")
+const Sbtnsound = document.querySelector("#btnsound")
+const Sasound = document.querySelector("#asound")
+const Ssecret = document.querySelector("#secret")
+
+for (let i = 0; i < $btns.length; i++) {
+	$btns[i].addEventListener("click", (e) => {
+		e.preventDefault()
+		Sbtnsound.play()
+	})
+}
+for (let i = 0; i < $as.length; i++) {
+	$as[i].addEventListener("click", () => {
+		Sasound.play()
+	})
+}
+
+$img.addEventListener("click", () => {
+	Ssecret.play()
+})
